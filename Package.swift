c8dccfe9e445d5a2bbb7e4cc9bddbfed05586f1e
gitbobobo <godbobo@aliyun.com>
2025-08-12 19:57:25 +0800

    update repository

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,12 +12,12 @@ let package = Package(
             name: "MPVPlayer",
             targets: [
                 "MPVPlayer"
-            ],
-        ),
+            ]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/mpvkit/MPVKit.git", exact: "0.39.0"),
-        .package(url: "https://github.com/malcommac/Repeat.git", .upToNextMajor(from: "0.6.0")),
+        .package(url: "https://github.com/mpvkit/MPVKit.git", .upToNextMajor(from: "0.40.0")),
+        .package(url: "https://github.com/malcommac/Repeat.git", .upToNextMajor(from: "0.6.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -28,7 +28,6 @@ let package = Package(
                 .product(name: "MPVKit", package: "MPVKit"),
                 .product(name: "Repeat", package: "Repeat")
             ]
-        ),
-
+        )
     ]
 )
